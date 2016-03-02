4643574794d5b6cb99870b4810a6a9cffe178e29
Adrien Devresse <adrien.devresse@epfl.ch>
2016-03-02 16:17:56 +0100

    Simplify BG/Q portability file

--- Modules/portability/BlueGenePortability.cmake
+++ Modules/portability/BlueGenePortability.cmake
@@ -29,26 +29,5 @@ endif()
 endif()
 
 
-# cmake > 3.0 add -Wl,-BDynamic for any linking argument
-# which does not match the library pattern *.a
-# this is annoying and cause the pkg_search_module _LIBRARIES to bug
-# in case of static linking
-# this macro fix this issue once for all 
-macro(fix_bgq_static_linking INPUT_LIST OUTPUT_LIST )
-        if("${CMAKE_VERSION}" VERSION_GREATER "3.0")
-
-		set(LOCAL_LIST)
-
-		LIST(APPEND LOCAL_LIST "-Wl,-Bstatic")
-		LIST(APPEND LOCAL_LIST "${INPUT_LIST}")
-
-		SET(${OUTPUT_LIST} ${LOCAL_LIST})
-	else()
-		set(${OUTPUT_LIST} "${INPUT_LIST}")
-
-	endif()
-
-endmacro()
-
 
 
