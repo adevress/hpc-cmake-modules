d29bd489f714ca8b47e53094c1b8c3c33cd1da6d
Adrien Devresse <adrien.devresse@epfl.ch>
2016-04-05 15:54:11 +0200

    Improve BlueGene portability support with needed HDF5/cmake static compilation options

--- Modules/portability/BlueGenePortability.cmake
+++ Modules/portability/BlueGenePortability.cmake
@@ -9,7 +9,6 @@ endif()
 if(BLUEGENE)
 	# define library type to static on BGQ
 	set(COMPILE_LIBRARY_TYPE "STATIC")
-	
 	## Blue Gene/Q do not support linking with MPI library when compiled with mpicc wrapper
         ## we disable any MPI_X_LIBRARY linking and rely on mpicc wrapper
 	set(MPI_LIBRARIES "")
@@ -19,7 +18,13 @@ if(BLUEGENE)
 	## static linking need to be forced on BlueGene
 	# Boost need a bit of tuning parameters for static linking
 	set(Boost_NO_BOOST_CMAKE TRUE)
-        set(Boost_USE_STATIC_LIBS TRUE)	
+    	set(Boost_USE_STATIC_LIBS TRUE)	
+
+	#enforce static linking for hdf5
+	set(HDF5_USE_STATIC_LIBRARIES TRUE)
+
+	## enforce static library discovery for modules
+	set(CMAKE_EXE_LINKER_FLAGS "-static")
 else()
 
 if(NOT DEFINED COMPILE_LIBRARY_TYPE)
@@ -27,7 +32,6 @@ if(NOT DEFINED COMPILE_LIBRARY_TYPE)
 endif()
 
 endif()
-
 
 
 
